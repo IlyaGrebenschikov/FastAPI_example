2c98e83a4d1df99cf6bab21c02682f79fda2185f
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-07-11 22:19:26 +0400

    add (connection, connection type)

---
+++ user_service/src/app/database/core/connection.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy import URL
+
+from user_service.src.app.common.types.database import SessionFactory
+
+
+def create_engine(url_obj: URL) -> AsyncEngine:
+    return create_async_engine(url_obj)
+
+
+def create_async_session_maker(engine: AsyncEngine) -> SessionFactory:
+    return async_sessionmaker(engine, class_=AsyncSession)
