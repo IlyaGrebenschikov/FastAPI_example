b2265974e18529ecb18096911ccc5e8d216ed8f0
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-08-04 07:59:17 +0400

    rework (init_dependencies), delete(engine_marker)

--- user_service/src/database/markers/database.py
+++ user_service/src/database/markers/database.py
@@ -1,6 +1,2 @@
-async def engine_marker() -> None:
-    yield NotImplemented
-
-
 async def session_marker() -> None:
     yield NotImplemented
--- user_service/src/settings/dependencies.py
+++ user_service/src/settings/dependencies.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
 
 from user_service.src.database.core.connection import create_engine, create_async_session_maker
-from user_service.src.database.markers.database import session_marker, engine_marker
+from user_service.src.database.markers.database import session_marker
 from user_service.src.core.settings import DatabaseSettings
 
 
 def init_dependencies(app: FastAPI, db_settings: DatabaseSettings) -> None:
-    app.dependency_overrides[create_engine(db_settings.get_url_obj)] = engine_marker
-    app.dependency_overrides[create_async_session_maker(Depends(engine_marker))] = session_marker
+    engine = create_engine(db_settings.get_url_obj)
+    app.dependency_overrides[create_async_session_maker(engine)] = session_marker
