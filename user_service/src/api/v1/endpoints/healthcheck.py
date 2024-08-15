ab3d85ea18ac7df769b90aafa350a04330db50e8
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-08-15 21:59:29 +0400

    add (healthcheck [response_description, description, summary])

--- user_service/src/api/v1/endpoints/healthcheck.py
+++ user_service/src/api/v1/endpoints/healthcheck.py
@@ -6,10 +6,13 @@ from user_service.src.common.dto.healthcheck import HealthCheckResponseSchema
 healthcheck_router = APIRouter(tags=["healthcheck"])
 
 
-@healthcheck_router .get(
-    "/healthcheck",
+@healthcheck_router.get(
+    '',
     status_code=status.HTTP_200_OK,
-    response_model=HealthCheckResponseSchema
+    response_model=HealthCheckResponseSchema,
+    response_description='Health resource',
+    description='Retrieves a health status of the application',
+    summary='Retrieves a health status of the application'
 )
 async def healthcheck_endpoint() -> dict[str, bool]:
     return {
