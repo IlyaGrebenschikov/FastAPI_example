2bfb3ea255e2a4c120071dd2d9ec6fdb11d63776
IlyaGrebenschikov <ilushagr22@gmail.com>
2025-11-03 18:48:39 +0400

    feat: updated db mixins and migration

--- src/infrastructure/database/models/mixins/with_guid.py
+++ src/infrastructure/database/models/mixins/with_guid.py
@@ -1,13 +1,18 @@
+import uuid
+
+from sqlalchemy import text
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (
     Mapped,
     mapped_column
 )
-from sqlalchemy.types import UUID
-
 
 class ModelWithIDMixin:
     id: Mapped[UUID] = mapped_column(
-        UUID,
+        UUID(as_uuid=True),
         primary_key=True,
+        default=uuid.uuid4,
+        server_default=text('gen_random_uuid()'),
         index=True,
+        nullable=False
     )
--- src/infrastructure/database/models/mixins/with_time.py
+++ src/infrastructure/database/models/mixins/with_time.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped
-
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 class ModelWithTimeMixin:
-    created_at: Mapped[datetime]
-    updated_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        onupdate=func.now(),
+        server_default=func.now()
+    )
