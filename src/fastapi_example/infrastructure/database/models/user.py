874ea7eceac713f38e98b2a84a618ad7ca9247b7
IlyaGrebenschikov <ilushagr22@gmail.com>
2026-03-01 03:27:44 +0400

    feat: Reworked user fields, migration

--- src/fastapi_example/infrastructure/database/models/user.py
+++ src/fastapi_example/infrastructure/database/models/user.py
@@ -1,11 +1,12 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 from .mixins import ModelWithTimeMixin, ModelWithIDMixin
 
 class UserModel(ModelWithIDMixin, ModelWithTimeMixin, Base):
-    username: Mapped[str]
-    email: Mapped[str]
-    password: Mapped[str]
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False, index=True)
+    password: Mapped[str] = mapped_column(Text, nullable=False)
 
     __tablename__ = "users"
